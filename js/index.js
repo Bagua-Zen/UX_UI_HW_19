fc5d41ebf0dc53f80c0960d9fb095cfdd073795c
Zen-Balance <152342873+Zen-Balance@users.noreply.github.com>
2024-06-20 22:02:08 -0700

    mod 19
    the background is now black upon loading.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 var load = document.querySelector("#loadingScreen");
 var page = document.querySelector("#page")
-
+var body = document.body
 window.addEventListener("load", function() {
 setTimeout(() => 
     {
      document.getElementById("loadingScreen").style.display = "none"; 
      document.getElementById("page").style.display = "block";
+     body.classList.add("bodyStyling")
     }, 4000);
 
 })
